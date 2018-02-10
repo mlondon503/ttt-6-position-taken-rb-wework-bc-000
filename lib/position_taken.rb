f55090c91219547a1114aedf1d48f0eda2529bf1
Michael London <michael.london@wework.com>
2018-02-10 21:38:39 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+ # index = index.to_int 
+  #index = index - 1
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else 
+    return true
+  end
+end
