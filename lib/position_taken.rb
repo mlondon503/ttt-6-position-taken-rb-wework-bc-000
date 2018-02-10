38666384bc07076b451ead4ec38176804cd8c6a1
Michael London <michael.london@wework.com>
2018-02-10 21:36:27 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,9 +3,11 @@
 def position_taken?(board, index)
   index = index.to_int 
   index = index - 1
-  if board[index] != " "
-    return true
-  else
+  if board[index] == " "
     return false
+  elsif board[index] == ""
+    return false
+  else 
+    return true
   end
 end
